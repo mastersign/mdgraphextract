b8e555ae7ec3e2a0ff5e2df6a13ac13c864d58d9
Tobias Kiertscher <dev@mastersign.de>
2014-08-14 19:15:37 +0200

    switched from tspec to tap-spec (tspec is an alias)

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 
 gulp.task('test', shell.task([
-	'tape ./tests/*.js | tspec'
+	'tape ./tests/*.js | tap-spec'
 ], { ignoreErrors: true }));
 
 gulp.task('autotest', function() {
