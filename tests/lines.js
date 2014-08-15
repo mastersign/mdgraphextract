1d6230008f776dbd69fe668a8b7286883dc44fe6
Tobias Kiertscher <dev@mastersign.de>
2014-08-15 17:10:14 +0200

    added test for lines in flow and non-flow mode

--- tests/lines.js
+++ tests/lines.js
@@ -13,7 +13,7 @@ test('Lines from undefined', function(t) {
 	t.end();
 });
 
-test('Lines from string', function(t) {
+test('Lines from string (flow)', function(t) {
 	var text = 'Hallo Welt\r\n Was geht? \n\nUnd tschüß.';
 	var expects = ['Hallo Welt', ' Was geht? ', '', 'Und tschüß.'];
 
@@ -28,12 +28,36 @@ test('Lines from string', function(t) {
 	});
 });
 
-test('Lines from buffer', function(t) {
-	var buffer = new Buffer('Hallo Welt\r\n Was geht? \n\nUnd tschüß.\n', 'utf-8');
+test('Lines from string (non-flow)', function(t) {
+	var text = 'Hallo Welt\r\n Was geht? \n\nUnd tschüß.';
+	var expects = ['Hallo Welt', ' Was geht? ', '', 'Und tschüß.'];
+
+	var result = [];
+	var s = lines(text);
+	var doRead = function() { 
+		var line = s.read();
+		if (line !== null) {
+			result.push(line);
+			return true;
+		} else {
+			return false;
+		}
+	};
+	s.on('readable', function() {
+		while(doRead());
+	});
+	s.on('end', function() {
+		t.deepEqual(result, expects);
+		t.end();
+	});
+});
+
+test('Lines from buffer (flow)', function(t) {
+	var buffer = new Buffer('Hallo Welt\r\n Was geht? \n\nUnd tschüß.\n', 'utf8');
 	var expects = ['Hallo Welt', ' Was geht? ', '', 'Und tschüß.', ''];
 
 	var result = [];
-	var s = lines(buffer);
+	var s = lines(buffer, 'utf8');
 	s.on('data', function(line) { 
 		result.push(line); 
 	});
@@ -43,14 +67,62 @@ test('Lines from buffer', function(t) {
 	});
 });
 
-test('Lines from stream', function(t) {
-	var stream = streamifier.createReadStream('Hallo Welt\r\n Was geht? \n\nUnd tschüß.\n', { encoding: 'utf-8' });
+test('Lines from buffer (non-flow)', function(t) {
+	var buffer = new Buffer('Hallo Welt\r\n Was geht? \n\nUnd tschüß.\n', 'utf8');
+	var expects = ['Hallo Welt', ' Was geht? ', '', 'Und tschüß.', ''];
+
+	var result = [];
+	var s = lines(buffer);
+	var doRead = function() { 
+		var line = s.read();
+		if (line !== null) {
+			result.push(line);
+			return true;
+		} else {
+			return false;
+		}
+	};
+	s.on('readable', function() {
+		while(doRead());
+	});
+	s.on('end', function() {
+		t.deepEqual(result, expects);
+		t.end();
+	});
+});
+
+test('Lines from stream (flow)', function(t) {
+	var stream = streamifier.createReadStream('Hallo Welt\r\n Was geht? \n\nUnd tschüß.\n', { encoding: 'utf8' });
 	var expects = ['Hallo Welt', ' Was geht? ', '', 'Und tschüß.', ''];
 
 	var result = [];
 	var s = lines(stream);
 	s.on('data', function(line) { 
-		result.push(line); 
+		result.push(line);
+	});
+	s.on('end', function() {
+		t.deepEqual(result, expects);
+		t.end();
+	});
+});
+
+test('Lines from stream (non-flow)', function(t) {
+	var stream = streamifier.createReadStream('Hallo Welt\r\n Was geht? \n\nUnd tschüß.\n', { encoding: 'utf8' });
+	var expects = ['Hallo Welt', ' Was geht? ', '', 'Und tschüß.', ''];
+
+	var result = [];
+	var s = lines(stream);
+	var doRead = function() { 
+		var line = s.read();
+		if (line !== null) {
+			result.push(line);
+			return true;
+		} else {
+			return false;
+		}
+	};
+	s.on('readable', function() {
+		while(doRead());
 	});
 	s.on('end', function() {
 		t.deepEqual(result, expects);
