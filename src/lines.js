3415e261ca2671943bc4ae32e6ff2d4ef0a16a68
Tobias Kiertscher <dev@mastersign.de>
2014-08-15 17:07:34 +0200

    added support for input encoding

--- src/lines.js
+++ src/lines.js
@@ -4,7 +4,6 @@ var split = require('split-stream');
 
 var TextSplitter = function(text) {
 	Readable.call(this, { objectMode: true });
-	var that = this;
 	this._text = text;
 	this._nl = /\r?\n/g;
 	this._p = 0;
@@ -12,25 +11,30 @@ var TextSplitter = function(text) {
 util.inherits(TextSplitter, Readable);
 
 TextSplitter.prototype._read = function() {
+	var part;
 	match = this._nl.exec(this._text);
 	if (match) {
-		this.push(this._text.slice(this._p, match.index));
+		part = this._text.slice(this._p, match.index);
+		this.push(part);
 		this._p = match.index + match[0].length;
 		this._nl.lastIndex = this._p;
 	} else {
-		this.push(this._text.slice(this._p, this._text.length));
+		part = this._text.slice(this._p, this._text.length);
+		this.push(part);
 		this.push(null);
 	}
 };
 
-var lines = function(input) {
+var lines = function(input, encoding) {
+	encoding = encoding || 'utf8';
 	if (input === null || input === undefined) {
 		return;
 	} else if (typeof(input) === 'string') {
 		return new TextSplitter(input);
 	} else if (input instanceof Buffer) {
-		return new TextSplitter(input.toString());
+		return new TextSplitter(input.toString(encoding));
 	} else if (input instanceof Readable) {
+		input.setEncoding(encoding);
 		return input.pipe(split.create());
 	} else {
 		throw 'Input not supported.';
