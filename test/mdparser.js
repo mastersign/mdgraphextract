1fbf9b12433b53578f382a77e9f64113134b2e8b
Tobias Kiertscher <tobias.kiertscher@de.bosch.com>
2015-11-30 14:27:30 +0100

    ported MdParser tests from tape to mocha

---
+++ test/mdparser.js
@@ -0,0 +1,205 @@
+/* globals require, describe, it */
+
+var assert = require('assert');
+var fs = require('fs');
+
+var MdParser = require('../src/mdparser');
+
+var checkObjectArray = function(result, expected) {
+	var i;
+	assert(result, 'result should be truthy');
+	assert(result instanceof Array, 'result should be an array');
+	assert.equal(result.length, expected.length, 'result has the right length');
+	for (i = 0; i < expected.length; i++) {
+		assert.deepEqual(result[i], expected[i], 'array item should be equal');
+	}
+};
+
+describe('MdParser', function () {
+
+	it('headlines', function(done) {
+		var expected = [
+			{ source: "Main Headline", 
+			text: "Main Headline", 
+			anchor: "main-headline",
+			level: 1, 
+			row: 8, column: 1 },
+			{ source: "Subsection",
+			text: "Subsection",
+			anchor: "subsection",
+			level: 2,
+			row: 11, column: 1 },
+			{ source: "(01) Headline {#first_hl}",
+			text: "(01) Headline",
+			anchor: "first_hl",
+			level: 1,
+			row: 20, column: 1 },
+			{ source: "1.2 _Headline_ **2**",
+			text: "1.2 Headline 2",
+			anchor: "headline-2",
+			level: 2, row: 22, column: 1 },
+			{ source: "Headline <http://www.endoftheinternet.com/>",
+			text: "Headline http://www.endoftheinternet.com/",
+			anchor: "headline-httpwww.endoftheinternet.com",
+			level: 3,
+			row: 25, column: 1 },
+			{ source: "Headline [hl1](#first_hl), [hl1][(01) Headline], [(01) Headline][], [(01) Headline] - []",
+			text: "Headline hl1, hl1, (01) Headline, (01) Headline - []",
+			anchor: "headline-hl1-hl1-01-headline-01-headline---",
+			level: 1,
+			row: 27, column: 1 },
+			{ source: "the last headline",
+			text: "the last headline",
+			anchor: "the-last-headline",
+			level: 4, 
+			row: 33, column: 1 },
+			{ source: "Subsection",
+			text: "Subsection",
+			anchor: "subsection_1",
+			level: 2,
+			row: 35, column: 1 },
+			{ source: "Subsection",
+			text: "Subsection",
+			anchor: "subsection_2",
+			level: 2,
+			row: 37, column: 1 }
+		];
+	
+		var p = new MdParser(fs.createReadStream('test/data/headlines.md'));
+		var result = [];
+		p.on('headline', function(hl) { result.push(hl); });
+		p.on('end', function() {
+			checkObjectArray(result, expected);
+			done();
+		});
+		p.resume();
+	});
+	
+	it('internal links', function(done) {
+		var expected = [
+			{ target: 'Headline', targetText: 'Headline', text: 'First Headline', row: 3, column: 1 },
+			{ target: 'Headline', targetText: 'Headline', text: 'Headline', row: 4, column: 5 },
+			{ target: 'Headline', targetText: 'Headline', text: 'Headline', row: 5, column: 5 },
+			{ target: 'Headline', targetText: 'Headline', text: 'links', row: 7, column: 10 },
+			{ target: 'Headline', targetText: 'Headline', text: 'one', row: 7, column: 31 },
+			{ target: 'Headline', targetText: 'Headline', text: 'Headline', row: 7, column: 47 },
+			{ target: 'Headline', targetText: 'Headline', text: 'last', row: 9, column: 23 },
+			{ target: 'Headline', targetText: 'Headline', text: 'very last 1', row: 15, column: 28 },
+			{ target: 'Headline', targetText: 'Headline', text: 'very last 2', row: 17, column: 25 }
+		];
+	
+		var p = new MdParser(fs.createReadStream('test/data/internal-links.md'));
+		var result = [];
+		p.on('internal-link', function(hl) { result.push(hl); });
+		p.on('end', function() {
+			checkObjectArray(result, expected);
+			done();
+		});
+		p.resume();
+	});
+	
+	it('links', function(done) {
+		var expected = [
+			{ text: 'http://www.theendoftheinternet.com/', 
+			url: 'http://www.theendoftheinternet.com/',
+			row: 1, column: 12 },
+			{ text: 'mailto:link@xyz.com', url: 'mailto:link@xyz.com',
+			row: 3, column: 23 },
+			{ text: 'URL', url: '../test.html', row: 4, column: 5 },
+			{ text: 'link', url: 'ftp://link', row: 8, column: 1 },
+			{ text: 'hidden', url: 'hidden.txt', row: 8, column: 43 },
+			{ text: 'http://www.example.com', url: 'http://www.example.com', row: 13, column: 1 },
+			{ text: 'link', url: 'link.txt', row: 18, column: 13 },
+			{ text: 'file://link2.txt', url: 'file://link2.txt', row: 18, column: 31 }
+		];
+	
+		var p = new MdParser(fs.createReadStream('test/data/links.md'));
+		var result = [];
+		p.on('link', function(hl) { result.push(hl); });
+		p.on('end', function() {
+			checkObjectArray(result, expected);
+			done();
+		});
+		p.resume();
+	});
+	
+	it('code', function(done) {
+		var expected = [
+			{ typ: 'start', row: 9, column: 1 },
+			{ text: '<code 1>', row: 9, column: 1 },
+			{ text: '', row: 10, column: 1 },
+			{ text: '// <code 3>', row: 11, column: 1 },
+			{ text: '  <code 3.1>', row: 12, column: 1 },
+			{ text: '    <code 3.1.1>', row: 13, column: 1 },
+			{ typ: 'end', row: 13, column: 21 },
+			{ typ: 'start', row: 15, column: 1 },
+			{ text: '<code 4>', row: 15, column: 1 },
+			{ text: '# <code 5>', row: 16, column: 1 },
+			{ text: '', row: 17, column: 1 },
+			{ typ: 'end', row: 17, column: 2 },
+			{ typ: 'start', row: 22, column: 1, codeAttributes: '' },
+			{ text: 'simple', row: 22, column: 1 },
+			{ text: '', row: 23, column: 1 },
+			{ text: 'fenced code', row: 24, column: 1 },
+			{ typ: 'end', row: 24, column: 12 },
+			{ typ: 'start', row: 28, column: 1, codeAttributes: '' },
+			{ text: '~~~', row: 28, column: 1 },
+			{ text: 'more fenced code', row: 29, column: 1 },
+			{ text: '~~~', row: 30, column: 1 },
+			{ typ: 'end', row: 30, column: 4 },
+			{ typ: 'start', row: 34, column: 1, codeAttributes: 'js' },
+			{ text: 'function() { /* fenced code with attributes */ }', row: 34, column: 1 },
+			{ typ: 'end', row: 34, column: 49 }
+		];
+	
+		var p = new MdParser(fs.createReadStream('test/data/code.md'));
+		var result = [];
+		p.on('code', function(hl) { result.push(hl); });
+		p.on('startCode', function(e) { e.typ = 'start'; result.push(e); });
+		p.on('endCode', function(e) { e.typ = 'end'; result.push(e); });
+		p.on('end', function() {
+			checkObjectArray(result, expected);
+			done();
+		});
+		p.resume();
+	});
+	
+	it('comments', function(done) {
+		var expected = [
+			{ text: ' a comment', inline: true, row: 2, column: 22 },
+			{ text: '@', inline: true, row: 2, column: 44 },
+			{ text: ' Comment 1 ', inline: true, row: 6, column: 1 },
+			{ text: ' Comment 2 ', inline: true, row: 7, column: 1 },
+			{ text: ' Comment 3 ', inline: true, row: 7, column: 19 },
+			{ typ: 'start', row: 9, column: 1 },
+			{ text: ' Start', inline: false, row: 9, column: 1 },
+			{ text: 'This is a comment --', inline: false, row: 10, column: 1 },
+			{ text: 'with no end -> ', inline: false, row: 11, column: 1 },
+			{ text: 'This is end ', inline: false, row: 12, column: 1 },
+			{ typ: 'end', row: 12, column: 16 },
+			{ typ: 'start', row: 14, column: 1 },
+			{ text: 'Whats going on', inline: false, row: 15, column: 1 },
+			{ text: '', inline: false, row: 16, column: 1 },
+			{ text: '\tcomment no code', inline: false, row: 17, column: 1 },
+			{ text: '\tcomment 2 no code', inline: false, row: 18, column: 1 },
+			{ text: '', inline: false, row: 19, column: 1 },
+			{ typ: 'end', row: 20, column: 4 },
+			{ text: 'comment', inline: true, row: 22, column: 9 },
+			{ typ: 'start', row: 22, column: 28 },
+			{ text: 'multiline ', inline: false, row: 23, column: 1 },
+			{ typ: 'end', row: 23, column: 14 }
+		];
+	
+		var p = new MdParser(fs.createReadStream('test/data/comments.md'));
+		var result = [];
+		p.on('comment', function(hl) { result.push(hl); });
+		p.on('startComment', function(e) { e.typ = 'start'; result.push(e); });
+		p.on('endComment', function(e) { e.typ = 'end'; result.push(e); });
+		p.on('end', function() {
+			checkObjectArray(result, expected);
+			done();
+		});
+		p.resume();
+	});
+
+});
